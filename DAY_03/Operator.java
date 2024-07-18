8573de72a5d43d0b7a32828bbe8dee7f24b8b6d2
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-07-18 18:28:34 +0530

    assignment operator

---
+++ DAY_03/Operator.java
@@ -0,0 +1,14 @@
+public class Operator {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 19;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(++a);
+        System.out.println(b--);
+    }
+}
+

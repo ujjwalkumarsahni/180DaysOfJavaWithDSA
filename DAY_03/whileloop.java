d875fcf4f544f83121ab5846ab0383eadf3b18da
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-07-19 18:01:49 +0530

    while

---
+++ DAY_03/whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main(String args[]){
+        int Num = 10;
+        int i = 1;
+        while(Num<=i){
+            System.out.println(i);
+            i+=1;
+        }
+    }
+}
+
+

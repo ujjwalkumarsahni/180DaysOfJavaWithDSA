beb2897f68aff3bc29bf0fda7ff0015c7dcee8e0
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-07-29 22:53:01 +0530

    Introduction to java

---
+++ HackerRankProblem/IntroductionToJava.java
@@ -0,0 +1,13 @@
+package HackerRankProblem;
+
+public class IntroductionToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        
+        System.out.println("Hello, Java.");
+    }
+}
+
+
+
+

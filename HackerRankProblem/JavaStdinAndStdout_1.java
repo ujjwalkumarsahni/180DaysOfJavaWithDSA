7bf16f0a2b0e3fb2b34806e1f3c3fa5c8f56dbf1
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-07-29 23:28:07 +0530

    Java Stdin and stdout 1

--- HackerRankProblem/JavaStdinAndStdout_1.java
+++ HackerRankProblem/JavaStdinAndStdout_1.java
@@ -2,7 +2,7 @@ package HackerRankProblem;
 
 import java.util.Scanner;
 
-public class JavaStdinAndStdout {
+public class JavaStdinAndStdout_1 {
      public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();

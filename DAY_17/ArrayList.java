501a177c7a7540de813e573cb8c14679dbc4158c
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-08-02 22:52:34 +0530

    Array List

---
+++ DAY_17/ArrayList.java
@@ -0,0 +1,22 @@
+import  java.util.ArrayList;
+
+
+public class ArrayList{
+    public static void main(String args[]){
+        ArrayList<Integer>  l1 = new ArrayList<>();
+        l1.add(2);
+        l1.add(5);
+        l1.add(4);
+        System.out.println(l1.get(0));
+
+        System.out.println(l1);
+
+        for(int i = 0;i<l1.size();i++){
+            System.out.println(l1.get(i));
+        }
+
+        System.out.println("Hello World");
+
+    }
+}
+

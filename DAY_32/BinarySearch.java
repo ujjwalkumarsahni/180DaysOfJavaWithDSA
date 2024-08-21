f5880314b523294a18971c49eb3b28a4228bd308
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-08-21 19:39:44 +0530

    Binary Search

--- DAY_32/BinarySearch.java
+++ DAY_32/BinarySearch.java
@@ -16,10 +16,28 @@ public class BinarySearch{
         }
         return false;
     }
+
+    static boolean RecursiveBinarySearch(int[] arr,int st,int end,int target){
+        if(st>end){
+            return false;
+        }
+        int mid = (st+end)/2;
+        if(target==arr[mid]){
+            return true;
+        }
+        else if(target<arr[mid]){
+            return RecursiveBinarySearch(arr,st,mid-1,target);
+        }
+        else{
+            return RecursiveBinarySearch(arr,mid+1,end,target);
+        }
+
+    }
     public static void main(String args[]){
         int[] arr = {1,2,3,4,5,6};
-        int target = 4;
-
+        int target = 10;
+        
         System.out.println(binarySearch(arr,target));
+        System.out.println(RecursiveBinarySearch(arr,0,arr.length-1,target));
     }
 }

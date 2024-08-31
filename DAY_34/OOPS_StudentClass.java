f8fcb88659b45ef309ecd7ab093009b2e5ae34ab
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-08-31 18:07:40 +0530

    OOPS Concept in java

---
+++ DAY_34/OOPS_StudentClass.java
@@ -0,0 +1,18 @@
+public class OOPS_StudentClass{
+    // public static class Student {
+    //     String Name;
+    //     int RollNo;
+    //     int Age;
+    // }
+    public static void main(String args[]){
+        Student obj = new Student();
+        obj.Name=  "Ujjwal Kumar";
+        obj.RollNo = 154;
+        obj.Age = 19;
+
+        System.out.println(obj.Name);
+        System.out.println(obj.RollNo);
+        System.out.println(obj.Age);
+    }
+}
+
